751378e2a58a63c4e682ee0e0f2606092e4e1533
enti <enti@enti.enti>
2022-02-04 11:32:23 +0100

    MongoDB

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+let http = require("http");
+
+let mongo_client = require("mongodb").MongoClient;
+
+let url = ("mongodb://localhost/");
+
+console.log("iniciando script mongo-http");
+
+mongo_client.connect(url, function(error, conn){
+	console.log("Dentro de MongoDB");
+
+	if (error){
+		console.log("ERROR!!!");
+		return;
+	}
+
+	let db = conn.db("tffhd");
+
+	let characters = db.collection("characters").find();
+
+	console.log(characters);
+
+	
+
+});
+
+/*
+
+http.createServer(function(req, res){
+	res.writeHead(200);
+
+		let saludo = "<h1>ola k ase</h1>";
+
+	res.end(saludo);
+
+}).listen(1095);
+
+*/
